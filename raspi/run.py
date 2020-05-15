ee55f346805cc340569f7457d8e2497bfb8081f7
brapru <smith.pruitt@gmail.com>
2020-05-15 21:35:15 +0100

    Added socketio functionality to the flask server. Added the HK711 class to interface with driver. Can now read weight from load cell sensor and push it live to the client side via flask socketio. Deleted the proto dir, created main and api directories, and split the relative routes.

--- raspi/run.py
+++ raspi/run.py
@@ -1,6 +1,8 @@
-from api import create_app
+from api import create_app, socketio
+from flask_socketio import SocketIO
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    #app.run(debug=True, host='0.0.0.0')
+    socketio.run(app, debug=True, host='0.0.0.0')
