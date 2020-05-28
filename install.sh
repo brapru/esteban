a2c04ee2d3b2250bf21c3dbcd035d9e1e971c137
Brandon Pruitt <brapru@Brandons-MacBook-Pro.local>
2020-05-28 19:35:14 -0400

    "Install script for dependencies"

---
+++ install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+ESTEBAN_OPENOCD='./mcu/openocd_cfg/bin/xpack-openocd-0.10.0-13-darwin-x64.tgz'
+
+function intro {
+        echo -e "\n\testeban dependency installation script"
+        echo -e "\t${YELLOW} _______  "
+        echo -e "\t\ . . . /"
+        echo -e "\t \ . . /"
+        echo -e "\t---------${NC}\n"
+}
+
+function outro {
+        echo -e "\n\testeban dependency installation script complete!"
+        echo -e "\t${YELLOW} _______  "
+        echo -e "\t\ . . . /"
+        echo -e "\t \ . . /"
+        echo -e "\t---------${NC}\n"
+}
+
+tool_exists(){
+	command -v "$1" >/dev/null 2>&1;
+}
+
+function check_brew {
+        echo -e "[${YELLOW}-${NC}] Checking for homebrew..."
+        if tool_exists brew; then        
+        	echo -e "[${GREEN}-${NC}] Brew installed."
+	else
+		echo "[${RED}!${NC}] Hombrew not installed. Grabbing the latest version."
+        	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	fi
+        echo -e "[${GREEN}*${NC}] Brew setup complete"      
+}
+
+function check_arm {
+        echo -e "[${YELLOW}-${NC}] Checking for ARM tools..."
+        if tool_exists arm-none-eabi-gcc; then        
+        	echo -e "[${GREEN}-${NC}] ARM tools installed."
+	else
+		echo -e "[${RED}!${NC}] ARM tools not installed. Grabbing the latest version."
+		brew tap ArmMbed/homebrew-formulae
+		brew install arm-none-eabi-gcc
+	fi
+        echo -e "[${GREEN}*${NC}] ARM tools setup complete"      
+}
+
+
+function check_stlink {
+        echo -e "[${YELLOW}-${NC}] Checking for ST Tools..."
+        if tool_exists st-info; then        
+        	echo -e "[${GREEN}-${NC}] ST Tools installed."
+	else
+		echo -e "[${RED}!${NC}] ST Tools not installed. Grabbing the latest version."
+		brew install stlink
+	fi
+        echo -e "[${GREEN}*${NC}] ST Tools setup complete"      
+}
+
+function get_openocd {
+	mkdir -p ~/opt
+	cp ${ESTEBAN_OPENOCD} ~/opt	
+	tar -xzf ~/opt/xpack-openocd-0.10.0-13-darwin-x64.tgz -C ~/opt 
+	ln -s ~/opt/xPacks/openocd/0.10.0-13/bin/openocd /usr/local/bin/openocd
+}
+
+function check_openocd {
+        echo -e "[${YELLOW}-${NC}] Checking for OpenOCD..."
+        if tool_exists openocd; then        
+        	echo -e "[${GREEN}-${NC}] OpenOCD installed."
+	else
+		echo -e "[${RED}!${NC}] OpenOCD not installed. Grabbing the latest version."
+		get_openocd	
+	fi
+        echo -e "[${GREEN}*${NC}] OpenOCD setup complete"      
+}
+
+function check_telnet {
+        echo -e "[${YELLOW}-${NC}] Checking for telnet..."
+        if tool_exists telnet; then        
+        	echo -e "[${GREEN}-${NC}] telnet installed."
+	else
+		echo -e "[${RED}!${NC}] telnet not installed. Grabbing the latest version."
+		brew install telnet
+	fi
+        echo -e "[${GREEN}*${NC}] telnet setup complete"      
+}
+
+function main {
+        intro
+        check_brew
+	check_arm
+	check_stlink
+	check_openocd
+	check_telnet
+	outro	
+}
+
+main
