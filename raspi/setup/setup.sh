233a7f2c92e1f2ab81becfcfa513e7c56799a550
Brandon Pruitt <brapru@brandons-mbp.lan>
2020-12-05 22:22:50 -0500

    updated setup script for raspi

--- raspi/setup/setup.sh
+++ raspi/setup/setup.sh
@@ -21,10 +21,31 @@ function update {
         apt-get upgrade -y
 }
 
+function build_electreban {
+        echo "need to build electreban"
+}
+
+function startup {
+        cp -r /home/pi/esteban/linux-armv7l-unpacked/ /usr/local/bin/esteban/
+        echo "#!/bin/bash\nDISPLAY=:0 /usr/local/bin/esteban/electreban" > /usr/local/bin/electreban
+       
+        # If we remotely access the pi, do not want to spawn new electreban process 
+        cat <<- EOF >> /home/pi/.bashrc
+        if [ "x${SSH_TTY}" = "x" ]; then
+                /usr/local/bin/electreban
+        fi
+        EOF
+        
+        
+        sed -i 's/#xserver-command=X/xserver-command=X -nocursor/g' /etc/lightdm/lightdm.conf
+}
+
 function main {
+        update
         edit_hostname
         edit_config
-        update
+        esteban
+        startup
         echo 'Rebooting to make changes in effect...'
         sleep 3
         reboot now
