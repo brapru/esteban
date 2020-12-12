39e8dd58db5640514ff5d459926d56f45263ba84
Brandon <no@gmail.com>
2020-12-12 12:26:09 -0500

    testing tests

--- raspi/tests/test_pour.py
+++ raspi/tests/test_pour.py
@@ -5,14 +5,14 @@ from unittest.mock import MagicMock, patch
 
 sys.modules['RPi.GPIO'] = MagicMock()
 sys.modules['spidev'] = MagicMock()
-sys.modules['HX711'] = MagicMock()
-#from pourover.pour import Pour
+from pourover.pour import Pour
 
+#@patch('loadcell.hx711.HX711')
 #@pytest.fixture
 #def init_pour():
 #    return Pour(40)
 
 class TestPour:
-    def test_init_message(self):
+    def test_init_message(self, init_pour):
         assert 1 == 1
         #assert init_pour.message == "starting a fresh brew"
