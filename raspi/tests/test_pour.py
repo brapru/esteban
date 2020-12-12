f4bf692d41dde0bf21f422b4ca6bc93ebdb43df4
brapru <smith.pruitt@gmail.com>
2020-12-12 12:31:30 -0500

    tests on rpi

--- raspi/tests/test_pour.py
+++ raspi/tests/test_pour.py
@@ -3,16 +3,12 @@ import pytest
 import unittest
 from unittest.mock import MagicMock, patch
 
-sys.modules['RPi.GPIO'] = MagicMock()
-sys.modules['spidev'] = MagicMock()
 from pourover.pour import Pour
 
-#@patch('loadcell.hx711.HX711')
-#@pytest.fixture
-#def init_pour():
-#    return Pour(40)
+@pytest.fixture
+def init_pour():
+    return Pour(40)
 
 class TestPour:
     def test_init_message(self, init_pour):
-        assert 1 == 1
-        #assert init_pour.message == "starting a fresh brew"
+        assert init_pour.message == "starting a fresh brew"
--- raspi/utils/__init__.py
+++ raspi/utils/__init__.py
@@ -1,7 +1,7 @@
 from api import hx, tempsensor
 
-def get_hx_weight(self):
+def get_hx_weight():
     return max(0, int(hx.get_weight(5)))
 
-def get_tempsensor(self):
+def get_tempsensor():
     return max(0, int(tempsensor.get_water_temp()))
