6e4acf9b8a55f087e0dd60f10b05f8a6ddabfc34
brapru <smith.pruitt@gmail.com>
2020-12-13 08:51:38 -0500

    test_stmspi pep8

--- raspi/tests/test_stmspi.py
+++ raspi/tests/test_stmspi.py
@@ -11,15 +11,17 @@ BOIL_OFF = 1
 sys.modules["spidev"] = MagicMock()
 from stmspi.stmspi import RpiController
 
+
 @pytest.fixture
 def rpi():
     return RpiController(0)
 
+
 class TestRpiController:
     def test_init(self, rpi):
-        assert rpi.led == { "id" : 1, "state" : OFF }
-        assert rpi.pump == { "id" : 2, "state" : OFF, "direction" : 0, "speed" : 0 }
-        assert rpi.boiler == { "id" : 3, "state" : OFF }
+        assert rpi.led == {"id": 1, "state": OFF}
+        assert rpi.pump == {"id": 2, "state": OFF, "direction": 0, "speed": 0}
+        assert rpi.boiler == {"id": 3, "state": OFF}
 
     def test_get_device_status(self, rpi):
         rpi._setDeviceState(rpi.led, ON)
@@ -34,17 +36,17 @@ class TestRpiController:
         assert rpi.getDeviceStatus(rpi.led) == expected_led
         assert rpi.getDeviceStatus(rpi.pump) == expected_pump
         assert rpi.getDeviceStatus(rpi.boiler) == expected_boiler
-    
+
     def test_set_device_state_boiler(self, rpi):
         with pytest.raises(ValueError):
             assert rpi._setDeviceState(rpi.boiler, 2)
-        
+
         rpi._setDeviceState(rpi.boiler, BOIL_OFF)
-        assert rpi.boiler == { "id" : 3, "state" : BOIL_OFF }
+        assert rpi.boiler == {"id": 3, "state": BOIL_OFF}
 
         rpi._setDeviceState(rpi.boiler, BOIL_ON)
-        assert rpi.boiler == { "id" : 3, "state" : BOIL_ON }
-   
+        assert rpi.boiler == {"id": 3, "state": BOIL_ON}
+
     def test_set_device_speed_pump(self, rpi):
         rpi._setDeviceSpeed(rpi.pump, 2)
         assert rpi.pump["speed"] == 2
@@ -68,7 +70,7 @@ class TestRpiController:
     def test_boiler_off(self, rpi):
         rpi.boiler_off()
         assert rpi.boiler["state"] == BOIL_OFF
-   
+
     def test_pump_on(self, rpi):
         rpi.pump_on()
         assert rpi.pump["state"] == ON
