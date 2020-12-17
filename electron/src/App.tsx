783b894892326d5eb1647d41384b7589ab45a43f
brapru <smith.pruitt@gmail.com>
2020-12-17 00:09:40 -0500

    still setting up env for react

--- electron/src/App.tsx
+++ electron/src/App.tsx
@@ -1,39 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../assets/icon.svg';
+import icon from '../assets/esteban_load.svg';
+import styles from './constants/styles.css'
 
 const Hello = () => {
   return (
     <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
+      <div className="Hola">
+        <img alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            esteban
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            esteban please
-          </button>
-        </a>
+      <div className="hola">
+        <h1>hóla</h1>
       </div>
     </div>
   );
