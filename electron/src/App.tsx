40da248eb577619a7a371793c1cb533b0694a946
brapru <smith.pruitt@gmail.com>
2020-12-17 20:19:50 -0500

    first loading page complete.

--- electron/src/App.tsx
+++ electron/src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../assets/esteban_load.svg';
-import styles from './constants/styles.css'
 
 const Hello = () => {
   return (
     <div>
       <div className="Hola">
-        <img alt="icon" src={icon} />
+        <img className="icon" alt="icon" src={icon} />
       </div>
-      <div className="hola">
-        <h1>hóla</h1>
+      <div className="text">
+        <h1 className="hola">hóla.</h1>
       </div>
     </div>
   );
